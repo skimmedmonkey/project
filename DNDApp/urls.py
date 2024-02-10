5e8495edbdc1285532f3d0426b6d19800022b14c
steven <kims7@oregonstate.edu>
2024-02-10 16:40:08 -0500

    Initial setup of django site and Monsters file

--- DNDApp/urls.py
+++ DNDApp/urls.py
@@ -19,4 +19,17 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
 ]
+
+from django.urls import include
+
+urlpatterns += [
+    path('Monsters/', include('Monsters.urls'))
+] 
+
+# Use static() to add URL mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
