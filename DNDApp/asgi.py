d7a49a17c8f1469bf40ccb3ac127c465043b9583
steven <kims7@oregonstate.edu>
2024-02-10 16:10:37 -0500

    Remove old apps, replace with new app

--- DNDApp/asgi.py
+++ DNDApp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DMApp project.
+ASGI config for DNDApp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DMApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DNDApp.settings')
 
 application = get_asgi_application()
